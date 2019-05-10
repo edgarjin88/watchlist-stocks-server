daac5ff238c56a18e1f097a57caa37f4b57ee5b5
edgarjin88 <edgar.jin88@gmail.com>
2019-05-10 13:17:55 +1000

    session record test

--- app.js
+++ app.js
@@ -3,29 +3,68 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express(); 
+//express assigend 
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// app.use(function(req, res, next){
+//   console.log('Custom middleware fired22');
+//   next(); 
+// })
+app.use(session({
+  resave: false, 
+  saveUninitialized: false, 
+  secret: 'password',
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}));
+
+
+
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('password'));
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+//somehow when I put middleware after router, following functions are not fired. 
+// app.use( function(req, res, next){
+//   console.log('Custom middleware fired2');
+//   console.log('current session infomation:', req.session);
+//   next(); 
+// } ) 
+
+
+//not fired here∆
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+} ); 
+
+// Does the mean every time this error handler used? 
+//To make error message and store even though we don't use it.  
+//that is whey this middleware was at the end of code. Probably, send it the message made to last middle ware( errorHandler)
 
 // error handler
 app.use(function(err, req, res, next) {
